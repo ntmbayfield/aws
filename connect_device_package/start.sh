a8429ba4c642432acdc4ed9c9ae00c1c20a8220a
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-09-08 00:51:43 -0700

    Update repo to include cc3220 .cert, private and public keys, root-CA.crt, and start.sh.

---
+++ connect_device_package/start.sh
@@ -0,0 +1,18 @@
+# stop script on error
+set -e
+
+# Check to see if root CA file exists, download if not
+if [ ! -f ./root-CA.crt ]; then
+  printf "\nDownloading AWS IoT Root CA certificate from Symantec...\n"
+  curl https://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem > root-CA.crt
+fi
+
+# install AWS Device SDK for NodeJS if not already installed
+if [ ! -d ./node_modules ]; then
+  printf "\nInstalling AWS SDK...\n"
+  npm install aws-iot-device-sdk
+fi
+
+# run pub/sub sample app using certificates downloaded in package
+printf "\nRunning pub/sub sample application...\n"
+node node_modules/aws-iot-device-sdk/examples/device-example.js --host-name=afw2hjpsnzg18.iot.us-west-2.amazonaws.com --private-key=cc3220.private.key --client-certificate=cc3220.cert.pem --ca-certificate=root-CA.crt --client-id=sdk-nodejs-7ce89a0d-6e00-49b6-8523-ca4de81c5d8d
